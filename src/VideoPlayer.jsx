2dbd366e8ea9981a969ae1b7ab6d00793f1dc7c7
PathFOSS <fossprogrammingprojects@proton.me>
2024-02-19 19:57:56 +0200

    Changed react-player

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import ReactPlayer from "react-player";
 
 const VideoPlayer = (props) => {
     return <div className="video-section-video">
-        <ReactPlayer className="react-player" controls={true} light={props.thumbnail} style={{borderRadius: "0.5rem", overflow: "hidden"}} url={props.video}/>
+        <ReactPlayer className="react-player" playing={true} controls={true} light={props.thumbnail} style={{borderRadius: "0.5rem", overflow: "hidden"}} url={props.video}/>
     </div>
 }
 export default VideoPlayer;
