4a2abe509926c0ad7e75ed9f956e1aaf2f91e881
PathFOSS <fossprogrammingprojects@proton.me>
2024-02-19 19:33:57 +0200

    Changed base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://pathfoss.github.io/ReactJS-PathFOSS-Website/"
 })
