70f9fba545fcfcf3cc7ee8aef3a500eeb54cbc1b
PathFOSS <email@email.com>
2024-04-16 23:50:02 +0300

    Improved navbar icons and smooth scroll

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,17 @@
+import InfoIcon from "./images/info-circle.svg";
+import GitHubIcon from "./images/github.svg";
+
 const Navbar = () => {
     return <nav>
         <h3><span>PathFOSS</span> Portfolio</h3>
-        <a href="#about">
-            <button>About</button>
-        </a>
+        <div>
+            <a className="img-btn" href="https://github.com/PathFOSS" rel="nofollow" target="_blank">
+                <img src={GitHubIcon}/>
+            </a>
+            <a className="img-btn" href="#about">
+                <img src={InfoIcon}/>
+            </a>
+        </div>
     </nav>
 }
 export default Navbar;
